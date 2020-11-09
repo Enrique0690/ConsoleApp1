4844cce8f489ef2960802207da8b7b5be1b65e8e
Enrique <enrique.reyesmarin@gmail.com>
2020-11-08 20:15:24 -0500

    Cambio 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,6 +14,9 @@ namespace ConsoleApp1
             Console.WriteLine("Hola mundo este es el primer mensaje");
             //Cambio 2//
             Console.WriteLine("La suma de 2+2 es 4");
+            //Cambio 3//
+            Console.WriteLine("La gente que lee bastante tiene un extenso repertorio de palabras");
+            
         }
     }
 }
