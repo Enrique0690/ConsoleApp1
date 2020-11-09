fa782dc194db8dbdcab1a32150457cb31f19dfa9
Enrique <enrique.reyesmarin@gmail.com>
2020-11-08 20:06:49 -0500

    Primer cambio

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Cambio 1//
+            Console.WriteLine("Hola mundo este es el primer mensaje");
         }
     }
 }
